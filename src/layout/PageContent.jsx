5d58d4f745ab62f07c44aea458d16a731e4df09e
yigit-ozcan <oozcan.yigitt@gmail.com>
2026-03-01 23:16:59 +0300

    implement PageContent layout container following NextJS pattern

--- src/layout/PageContent.jsx
+++ src/layout/PageContent.jsx
@@ -1,5 +1,14 @@
+import { Switch, Route } from "react-router-dom";
+import HomePage from "../pages/HomePage";
+
 function PageContent() {
-  return <footer className="w-full"></footer>;
+  return (
+    <main className="flex-1">
+      <Switch>
+        <Route exact path="/" component={HomePage} />
+      </Switch>
+    </main>
+  );
 }
 
 export default PageContent;
